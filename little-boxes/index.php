72aed3b777fa94249abe3273d09fe8e0462bc131
Parker Smith <parker@parkersmith.us>
2016-06-29 14:20:12 -0600

    Setup little boxes

---
+++ little-boxes/index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Little Boxes</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        Little boxes on the hillside,
+        Little boxes made of ticky tacky,
+        Little boxes on the hillside,
+        Little boxes all the same.
+        There's a green one and a pink one
+        And a blue one and a yellow one,
+        And they're all made out of ticky tacky
+        And they all look just the same
+    </div>
+</body>
+</html>
