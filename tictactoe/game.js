035786cdb41b51d11d3f7ed185f9c065b6fd2313
Parker Smith <parker@parkersmith.us>
2016-07-20 15:26:38 -0600

    stuff

--- tictactoe/game.js
+++ tictactoe/game.js
@@ -1,17 +1,58 @@
-var currentPlayer = 'X';
+var currentPlayerSymbol = 'X';
+var currentPlayerClass = 'player1Marker';
 var xGameTotal = 0;
 var oGameTotal = 0;
 var winsArray = [7, 56, 448, 73, 146, 292, 273, 84]; // Wins condition values
+var player1Name = '';
+var player2Name = '';
+var player1Symbol = 'X';
+var player2Symbol = 'O';
+var player1 = {
+    name: '',
+    symbol: 'X',
+    class: 'player1Marker',
+    wins: 0
+}
+var player2 = {
+    name: '',
+    symbol: 'O',
+    class: 'player2Marker',
+    wins: 0
+};
+
+var currentPlayer = player1;
 
 function initialize()
 {
+    // Get the player names
+    // player1Name = prompt('Please enter player 1 name');
+    // player2Name = prompt('Please enter player 2 name');
+    player1.name = 'Parker';
+    player2.name = 'Bob';
 
+    // Set the names in the score boxes.
+    document.getElementById('player1Name').innerHTML = player1.name;
+    document.getElementById('player2Name').innerHTML = player2.name;
+
+    drawSymbols();
+}
+
+function drawSymbols()
+{
+    document.getElementById('player1Icon').innerHTML = player1.symbol;
+    document.getElementById('player2Icon').innerHTML = player2.symbol;
+
+    drawScore();
+}
+
+function drawScore()
+{
+    document.getElementById('player1Wins').innerHTML = 'Wins: ' + player1.wins;
+    document.getElementById('player2Wins').innerHTML = 'Wins: ' + player2.wins;
 }
 
 function playerMoved(id, value)
 {
-    //alert(id + ' ' + value);
-
     // place a marker
     changeMarker(id);
 
@@ -19,7 +60,7 @@ function playerMoved(id, value)
     updatePlayerTotal(value);
 
     // check for winner
-    checkForWinner(currentPlayer === 'X' ? xGameTotal : oGameTotal);
+    checkForWinner(currentPlayerSymbol === 'X' ? xGameTotal : oGameTotal);
 
     // switch players
     changePlayer();
@@ -27,18 +68,20 @@ function playerMoved(id, value)
 
 function changeMarker(box)
 {
-    box.innerHTML = currentPlayer === 'X' ? 'X' : 'O';
+    box.innerHTML = currentPlayerSymbol;
+    box.className = box.className + ' ' + currentPlayerClass;
 }
 
 function changePlayer()
 {
     // Swap the current player.
-    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+    currentPlayerSymbol = currentPlayerSymbol === 'X' ? 'O' : 'X';
+    currentPlayerClass = currentPlayerClass === 'player1Marker' ? 'player2Marker' : 'player1Marker';
 }
 
 function updatePlayerTotal(value)
 {
-    if (currentPlayer === 'X')
+    if (currentPlayerSymbol === 'X')
     {
         xGameTotal += value;
     }
